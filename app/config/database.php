293513e0a62ac45c85def1b5f0b58168b30c947f
jvva45 <joaovieiraat@gmail.com>
2025-12-18 18:06:47 -0300

    Remove senha do banco de dados do repositório

--- app/config/database.php
+++ app/config/database.php
@@ -1,7 +1,7 @@
 <?php
 define ('DB_HOST', 'localhost');
-define ('DB_NAME', 'desafio_webbrain');
+define ('DB_NAME', 'lista_tarefas');
 define ('DB_USER', 'root');
-define ('DB_PASS', 'Joaovieira45');
+define ('DB_PASS', '');
 
 $conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die ('Não foi possível conectar ao banco de dados.');
